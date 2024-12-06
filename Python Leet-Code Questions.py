ccaf79564460de3bbb24bf96a2821f8ae43ff210
Piyush Sharma <piyushsharmarj14@gmail.com>
2024-12-06 22:09:31 +0530

    Leet-Code Questions.py

--- Python Leet-Code Questions.py
+++ Python Leet-Code Questions.py
@@ -8,7 +8,7 @@
 #6.)three sum
 
 #1.) Vaild anagram or not....
-'''s="anagram"
+s="anagram"
 t="nagaram"
 dict1={}
 for char in s:
@@ -25,11 +25,11 @@ for char in t:
 if(dict1==dict2):
     print("anagram")
 else:
-    print("not anagram") '''
+    print("not anagram") 
 
 
 #2.)isomorphic or not....
-'''s="pep"
+s="pep"
 y="ziz"
 s_dict={}
 y_dict={}
@@ -43,10 +43,10 @@ if(len(s)==len(y)):
 if(x==0):
     print("isomorphic")
 else:
-    print("not isomorphic")'''
+    print("not isomorphic")
  
 # Second method to check ismorphic or not with using zip function...
-'''s="zac"
+s="zac"
 y="hey"
 dict1={}
 for i in zip(s,y):
@@ -54,11 +54,11 @@ for i in zip(s,y):
     if(s1 in dict1 and dict1[s1]!=s2):
        print(" Not isomorphic")
        dict1[s1]=s2
-print("isomorphic")'''
+print("isomorphic")
 
 
 #3.) Question name => Vaild Parenthesis..
-'''data="{[]}"
+data="{[]}"
 mylist=[]
 x=0
 for char in data:
@@ -77,11 +77,11 @@ for char in data:
 if(x==0 and len(mylist)==0):
   print("vaild")
 else:
-  print("invaild")'''
+  print("invaild")
 
 
 #4.)#time-to-buy-and-sell-stock question with using min and max function....
-'''mylist=[7,1,5,3,6,4]
+mylist=[7,1,5,3,6,4]
 profit=0
 min_price=mylist[0]
 for i in range(len(mylist)):
@@ -91,10 +91,10 @@ for i in range(len(mylist)):
     #print(min_price,mylist[i])
     min_price=min(min_price,mylist[i])
 
-print(profit)'''
+print(profit)
 
 # Input: List of stock prices
-'''prices = [7, 1, 5, 3, 6, 4]
+prices = [7, 1, 5, 3, 6, 4]
 max_profit = 0
 min_price = prices[0]
 i = 1
@@ -106,22 +106,22 @@ while i < len(prices):
         if(profit > max_profit):
             max_profit = profit
     i += 1
-print("The maximum profit is:", max_profit)'''
+print("The maximum profit is:", max_profit)
 
 
 #two sum ..
-'''nums=[1,2,3,7,11,15]
+nums=[1,2,3,7,11,15]
 target=9
 for x in range(0,len(nums)):
     print(x,nums[x])
     for y in range(x+1,len(nums)):
      if(nums[x]+nums[y]==target):
         print(x,y)
-        break'''
+        break
 
 
 #three sum..
-'''nums = [-1, 0, 1, 2, -1, -4]
+nums = [-1, 0, 1, 2, -1, -4]
 nums.sort()
 for i in range(0,len(nums)):
    j=i+1
@@ -133,26 +133,9 @@ for i in range(0,len(nums)):
       elif((nums[i]+nums[j]+nums[k]>0)):
         k+-1
       else:
-        j+=1'''
+        j+=1
 
-
-'''nums = [-1, 0, 1, 2, -1, -4]
-nums.sort()
-for i in range(0,len(nums)):
-    j=i+1
-    k=len(nums)-1
-    while(j<k):
-        if(nums[i]+nums[j]+nums [k]==0):
-            print(i,j,k)
-            j+=1
-        elif(nums[i]+nums[j]+nums[k]>0):
-            k+-1
-        else:
-            j+=1'''
-
-       
-
-
+#Three Sum second method..       
 nums = [-1, 0, 1, 2, -1, -4]
 nums.sort()
 
@@ -174,4 +157,23 @@ for i in range(len(nums)):
             k -= 1
         else:
             j += 1
+
+
+#Three sum close code leetcode..
+nums=[-1,2,1,-4]
+nums.sort()
+target=1
+diff=float('inf')
+for i in range(0,len(nums)-2):
+  start=i+1
+  end=len(nums)-1
+  while start<end:
+    t_sum=nums[i]+nums[start]+nums[end]
+    if abs(t_sum-target) < abs(target-diff):
+      dif=t_sum
+    if(t_sum<target):
+      start+=1
+    elif(t_sum>target):
+      end-=1
+print(t_sum)
 
